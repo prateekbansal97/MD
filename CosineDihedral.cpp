6195cb94904c1c9dbb9c338a8d019a0783488e07
Prateek Bansal <prateekbansal97@gmail.com>
2025-12-07 12:44:17 -0600

    Fixed CosineDihedral

--- CosineDihedral.cpp
+++ CosineDihedral.cpp
@@ -1,8 +1,9 @@
 #include "atom.h"
 #include "CosineDihedral.h"
+#include <cmath>
 
 double CosineDihedral::return_energy(double angle)
 {
-    double energy = Dihedral_force_constant*(angle - Dihedral_Phase)*(angle - Dihedral_Phase);
+    double energy = Dihedral_force_constant * (1.0 + std::cos(Dihedral_Periodicity * angle - Dihedral_Phase));
     return energy;
 }
